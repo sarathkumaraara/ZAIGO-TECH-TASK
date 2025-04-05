0d6a9ef45cf251ccfef8e4e23a6a54a96c7c15a2
sarathkumaraara <165144755+sarathkumaraara@users.noreply.github.com>
2025-04-05 18:26:03 +0530

    Update mongoDbConfig.js

--- backend/config/mongoDbConfig.js
+++ backend/config/mongoDbConfig.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async() => {
   try{
-    const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING_DEV
+    const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING_DEV  ||  "mongodb://localhost:27017/"
     await mongoose.connect(DB_CONNECTION_STRING,{
-      dbName: process.env.DB_NAME
+      dbName: process.env.DB_NAME || "task_management"
     })
   }
   catch(error){
